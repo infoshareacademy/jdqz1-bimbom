1b622c178725d8825a50eaef425b5d12e1fc3725
Kasia.Michalowska <katrmich@gmail.com>
2018-02-11 20:46:06 +0100

    add autoTests

---
+++ autoTests/src/main/java/com/infoshare/AppTest.java
@@ -0,0 +1,18 @@
+package com.infoshare;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTest {
+
+
+}
